67d987bcb7ebda5dccd1add379db4b2d0fa4956e
Kingsley1331 <kingankomah@hotmail.co.uk>
2018-06-30 10:34:22 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const  express = require('express');
+const app = express();
+
+app.use(express.static('client/build'));  
+app.listen(8080, function () { 
+    console.log('Express server listening on port ' + 8080);
+});
